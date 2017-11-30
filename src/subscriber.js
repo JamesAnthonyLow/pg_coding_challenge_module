732a57f6b35f433e93650c585578a70e1dc4d363
JamesAnthonyLow <jamesalowenthal@gmail.com>
2017-11-30 18:14:26 -0500

    add copyFields and tests

--- src/subscriber.js
+++ src/subscriber.js
@@ -3,6 +3,17 @@ import getConfigFromPath from './getConfigFromPath';
 import Validator from './validate';
 
 class Subscriber {
+  _copyFields(params, fields) {
+    let field;
+    if (typeof fields !== 'undefined') {
+      for (let i = 0; i < fields.length; i += 1) {
+        field = Object.keys(fields[i]);
+        if (typeof params[field] !== 'undefined') {
+          this[field] = params[field];
+        }
+      }
+    }
+  }
   constructor(params) {
     const subscriberSchema = getConfigFromPath(Conf.subscriber);
     const [valid, err] = Validator.checkSchema(subscriberSchema, params);
@@ -10,6 +21,8 @@ class Subscriber {
       throw new Error(`Invalid input: ${err}`);
     }
     this.pricingSchema = getConfigFromPath(Conf.pricing);
+    this._copyFields(params, subscriberSchema.required);
+    this._copyFields(params, subscriberSchema.optional);
   }
 }
 
--- src/subscriber.spec.js
+++ src/subscriber.spec.js
@@ -21,4 +21,26 @@ describe('Subscriber', () => {
       new Subscriber(subscriberParams);
     }).toThrow(new Error('Invalid input: required field age undefined'));
   });
+  it('copies required fields', () => {
+    const subscriberParams = { name: 'James', age: 26, gender: 'male' };
+    const s = new Subscriber(subscriberParams);
+    expect(s.name).toBe('James');
+    expect(s.age).toBe(26);
+    expect(s.gender).toBe('male');
+  });
+  it('copies optional fields', () => {
+    const subscriberParams = { 
+      name: 'James', 
+      age: 26, 
+      gender: 'male', 
+      hasAllergies: true,
+      hasHeartDisease: true
+    };
+    const s = new Subscriber(subscriberParams);
+    expect(s.name).toBe('James');
+    expect(s.age).toBe(26);
+    expect(s.gender).toBe('male');
+    expect(s.hasAllergies).toBe(true);
+    expect(s.hasHeartDisease).toBe(true);
+  });
 });
