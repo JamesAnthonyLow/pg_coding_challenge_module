6d98a472f1f8b6a391fa0fc9776ba0bafa9821ca
JamesAnthonyLow <jamesalowenthal@gmail.com>
2017-11-30 15:13:51 -0500

    add test cases

--- src/validate.spec.js
+++ src/validate.spec.js
@@ -1,21 +1,31 @@
-import Validator from './validate' 
+import Validator from './validate';
 
 const schema = {
-  required:[
-    {name: "String"},
-    {age: "Number"},
-    {gender:"boolean"}
+  required: [
+    { name: 'string' },
+    { age: 'number' },
+    { gender: 'string' },
   ],
   optional: [
-    {hasAllergies:"boolean"},
-    {hasSleepApnea:"boolean"},
-    {hasHeartDisease:"boolean"}
-  ]
-}
+    { hasAllergies: 'boolean' },
+    { hasSleepApnea: 'boolean' },
+    { hasHeartDisease: 'boolean' },
+  ],
+};
 
 describe('checkSchema', () => {
-  it ('returns false if any files are missing', () => {
-    let subscriber = { name: "James" };
+  it('returns false if any fields are missing', () => {
+    const subscriber = { name: 'James' };
+    expect(Validator.checkSchema(schema, subscriber)).toBe(false);
+  });
+  it('returns true if all required fields are present', () => {
+    const subscriber = { name: 'James', gender: 'Male', age: 26 };
+    expect(Validator.checkSchema(schema, subscriber)).toBe(true);
+  });
+  it('returns false if the type is incorrect for an optional field', () => {
+    const subscriber = {
+      name: 'James', gender: 'Male', age: 26, hasAllergies: 'true',
+    };
     expect(Validator.checkSchema(schema, subscriber)).toBe(false);
   });
 });
