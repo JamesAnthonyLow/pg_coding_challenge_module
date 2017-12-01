4a03e39528c6c5a79822c5befe284b7d7b947c9a
JamesAnthonyLow <jamesalowenthal@gmail.com>
2017-12-01 00:08:15 -0500

    change index to show example

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-import Conf from './config';
-console.log(Conf);
+import Subscriber from './subscriber';
+const kelly = new Subscriber({
+  name: 'Kelly', age: 50, gender: 'female', hasAllergies: true,
+});
+const Josh = new Subscriber({
+  name: 'Josh', age: 40, gender: 'male', hasSleepApnea: true,
+});
+const Brad = new Subscriber({
+  name: 'Brad', age: 20, gender: 'male', hasHeartDisease: true,
+});
+
+kelly.calculatePricing();
+console.log(`${kelly.name} - $${kelly.price.toFixed(2)}`);
+Josh.calculatePricing();
+console.log(`${Josh.name} - $${Josh.price.toFixed(2)}`);
+Brad.calculatePricing();
+console.log(`${Brad.name} - $${Brad.price.toFixed(2)}`);
